2570a9529e91a5c2f3da9e7262a03b0a2d53a870
Shivam Lohiya <shivam.lohiya@programmers.io>
2021-12-27 11:10:13 +0530

    frist commit

---
+++ src/components/Task.js
@@ -0,0 +1,10 @@
+import {FaTimes} from 'react-icons/fa'
+
+export const Task = ({task, onDelete, onToggle}) => {
+    return (
+        <div  className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={()=>onToggle(task.id)}>
+            <h3 key={task.id}>{task.text} <FaTimes onClick={()=>onDelete(task.id)} style={{ color:'red', cursor:'pointer' }}/> </h3>
+            <p>{task.day}</p>
+        </div>
+    )
+}
